a36e7af730cea1ee9a54d8c1377838a0a197d232
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-06-16 15:52:07 +0200

    add changes

--- src/Es1.java
+++ src/Es1.java
@@ -1,3 +1,5 @@
+import java.sql.Array;
+
 public class Es1 {
     public static void main(String[] args) {
 
@@ -8,8 +10,18 @@ public class Es1 {
         return num1 * num2;
     }
 
-    public static void concatenation(String stringa, num1) {
-        System.out.println("La stringa è: " + sum);
+    public static void concatenation(String stringa, int num1) {
+        System.out.println("La stringa è: " + num1);
+    }
+
+    public static void concatena(String[] array, String testo) {
+    String[]= new String[6];
+    new array[0]=array(0);
+    new array[1]=array(1);
+    new array[2]=array(2);
+    new array[3]=array(3);
+    new array[4]=array(4);
+    new array[5]=array(5);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import.java.util.Arrays
 public class Main {
+
+
     public static void main(String[] args) {
-        public static int multiplication(int num1, int num2){
-            return num1*num2;
+
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static String concatenation(String string, int num) {
+        return string + num;
+    }
+        String[] Arrays = {"de", "re", "Je", "TR", "WE"};
+        String string = "TY";
+
     }
 }
